4265597ecafc83ce26394d6b8ed5a3e3a85ea6ce
Aditya Lokesh <adi@MacBook-Pro.local>
2025-04-13 16:43:27 +0530

    final final

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from zoneinfo import ZoneInfo
